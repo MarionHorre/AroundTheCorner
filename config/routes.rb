6976c7d819f23de96f5099d79405a8b048fe3596
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-01 11:35:43 +0200

    routes finished, maybe will be changed for modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/recap", to: "pages#recap"
+  get "/districts", to: "districts#index"
+  post "/districts", to: "districts#search"
+  get "/houses", to: "houses#index"
+  get "/houses", to: "houses#show" # vérifier comment faire la show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
