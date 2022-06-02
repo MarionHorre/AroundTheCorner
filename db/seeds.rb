9832c6cad57a2557172a20e50db74bc5f2913475
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-02 12:54:36 +0200

    seed pharmacie fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,12 +43,37 @@ transport_vlille = type_transport_vlille.map do |element|
   latitude = element["geometry"]["coordinates"][1]
   # interest = Interest.new(address: @adresse, longitude: @longitude, latitude: @latitude, type: @vlille )
   city_name = polygone_maker.which_city(latitude, longitude)
-  p city_name
   unless city_name.nil?
     district = District.find_by(name: city_name)
     interest = Interest.create!(address: adresse, longitude: longitude, latitude: latitude, type: vlille, district: district)
   end
 end
-p transport_vlille.size
+
+# seeds Interest : Santé
+pharmacie = Type.create(category: 'santé', name: 'pharmacie')
+hopital = Type.create(category: 'santé', name: 'hôpital')
+veterinaire = Type.create(category: 'santé', name: 'vétérinaire')
+
+# seeds Interest : Santé => Pharmacies
+
+filepath_pharmacie = "db/data-mel/health/pharmacies.json"
+type_health_pharmacie = JSON.parse(File.read(filepath_pharmacie))
+
+health_pharmacie = type_health_pharmacie.map do |element|
+  adresse = element["fields"]["adresse"]
+  longitude = element["geometry"]["coordinates"][0]
+  latitude = element["geometry"]["coordinates"][1]
+  # interest = Interest.new(address: @adresse, longitude: @longitude, latitude: @latitude, type: @vlille )
+  city_name = polygone_maker.which_city(latitude, longitude)
+  unless city_name.nil?
+    district = District.find_by(name: city_name)
+    interest = Interest.create!(address: adresse, longitude: longitude, latitude: latitude, type: pharmacie, district: district)
+  end
+end
+
+# seeds Interest : Santé => Hopitaux
+
+# seeds Interest : Santé => Vétérinaies
+
 puts 'Finished!'
 # Interest.create(adresse, )
