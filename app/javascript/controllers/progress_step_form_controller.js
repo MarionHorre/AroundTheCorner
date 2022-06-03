581f4c208e1db96246d31e16514eef475636a3f8
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-03 12:26:15 +0200

    next button working

---
+++ app/javascript/controllers/progress_step_form_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus"
+
+// export default class extends Controller {
+//   static targets = [ "district" ]
+
+//   // connect() {
+//   //   console.log("test")
+//   // }
+
+//   removeButton(event) {
+//     console.log(this.element)
+//     this.districtTarget.classList.add("disable")
+//   }
+
+// }
