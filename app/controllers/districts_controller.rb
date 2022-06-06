888b9c3df1d246eb2a4046d68775e6514625725e
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-06 11:28:04 +0200

    params du form dispo pour le district Controller

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -2,6 +2,10 @@ class DistrictsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
+    # session[:user] = SecureRandom.base58 #Generate a token key for the user without altering the DB
+
+    # on récupère les params
+    @district_params = session[:params]
     @districts = District.first(3)
   end
 
