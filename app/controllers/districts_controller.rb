b033f3943fdb5dcb4082b9b1c3cdef8c65345675
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-06 15:00:24 +0200

    ça fonctionne :D enfin je crois

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -2,10 +2,8 @@ class DistrictsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-
     # on récupère les params
     @district_params = session[:params]
-
 
     # crée une hash par catégorie disposant des params du form
     transports = session[:params]["transport"]
@@ -13,34 +11,34 @@ class DistrictsController < ApplicationController
     hobbies = session[:params]["hobbies"]
     sante = session[:params]["santé"]
 
-    params_array = [transports, vie_quotidienne, hobbies, sante]
-
-    # autre idée
-    # on itère sur chaque key/value des params
-    # si true on
-
-    districts = District.all
     # on va itéré sur chaque district
+    districts = District.all
 
-    quartier_value = 0
-    raise
-    # each_with_index pour itérer avec l'index de la array ?
+    districts_points = []
 
     districts.each do |district|
       # on itère sur chaque type d'un district
+      district_value = 0
       district.types.each do |type|
         # si category == true dans le form => ajoute 1 à la valeur du quartier sinon 0
-        type.name == transports.each_value { |value| p value } ? quartier_value += 1 : "0"
-        # raise
+        # on compare le type.name aux params données par le visiteur
+        transports.nil? ? "" : ( transports.key?(type.name) ? district_value += 1 : "")
+        vie_quotidienne.nil? ? "" : ( vie_quotidienne.key?(type.name) ? district_value += 1 : "")
+        hobbies.nil? ? "" : ( hobbies.key?(type.name) ? district_value += 1 : "")
+        sante.nil? ? "" : ( sante.key?(type.name) ? district_value += 1 : "" )
       end
+      districts_points << [district_value, district.name]
     end
 
+    top_three_districts_sorted_reversed = districts_points.sort!.last(3).reverse
 
-    p "this is the district value #{quartier_value}"
-    @top_3_districts = District.first(3)
-    # raise
-    # @districts = District.first(3)
+    district_instances = []
 
+    top_three_districts_sorted_reversed.each do |name|
+      district_instances << District.find_by(name: name[1].to_s)
+    end
+
+    @top_3_districts = district_instances
   end
 
   # def search_flat
