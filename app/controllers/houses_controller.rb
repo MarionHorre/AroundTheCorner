71515670373d7acefb28fbf4d7563d9a75e8dd9c
virginie serrure <virginie.serrure1803@gmail.com>
2022-06-06 15:23:31 +0200

    add scraping

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,45 @@
 class HousesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
+  require 'open-uri'
+  require 'nokogiri'
+
+  def index
+    @houses = House.all
+
+    # récupérer les bons paramètres pour district et price
+    # district = district.name
+    # price = house.price
+    # ("https://lalilloise-immobilier.fr/annonces/?type=&localite=#{district}&prix=#{price}")
+
+    @results = []
+
+    html_content = URI.open("https://lalilloise-immobilier.fr/annonces/")
+    html_doc = Nokogiri::HTML(html_content)
+    html_doc.search('.card').each do |element|
+    square_meter = element.search(".annonce-infos li").first.text
+    room_number = element.search(".annonce-infos li")[1].text
+    name = element.search(".card-content .cat-name").text
+    description = element.search(".annonce-accroche").text
+    url_image = element.search(".attachment-annonce-thumb").attr('data-src').value
+      if (square_meter.to_i >= 100) && (room_number.to_i >= 5)
+        @results << [name: name, description: description, url_image: url_image, square_meter: square_meter, room_number: room_number]
+      end
+    end
+  end
 end
+
+
+
+# methode pour avoir seulement les annonces avec ou + le nombres de pièces demandés
+# @room_numbers.each do |room|
+
+# end
+
+# methode pour avoir seulement les annonces avec ou + le nombres de m² demandés
+# @square_meters.each do |meter|
+#   if
+#     p meter
+#   end
+# end
+
+# if room.to_i >= 3 && meter.to_i >= 70
