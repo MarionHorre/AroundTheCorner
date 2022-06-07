493eb2c7d6d619fc6fa9595165354ff74263bec1
Marion Horre <marion.horre@gmail.com>
2022-06-07 12:42:40 +0200

    Non necessary route for houses show removed

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,20 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-static targets = ["test", "normal"]
+static targets = ["big", "normal"]
 
   connect() {
     console.log("hello from navbar_controller!")
-    console.log(this.testTarget)
+    console.log(this.bigTarget)
     console.log(this.normalTarget)
   }
 
   display(event) {
-    this.testTarget.classList.remove('d-none')
+    this.bigTarget.classList.remove('d-none')
     console.log('display')
   }
 
   close(event) {
-    this.testTarget.classList.add('d-none')
+    this.bigTarget.classList.add('d-none')
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "/districts", to: "districts#index"
   post "/districts", to: "districts#search"
   get "/houses", to: "houses#index"
-  get "/houses", to: "houses#show", as: "houses_details" # vérifier comment faire la show
+  get "/houses", to: "houses#show" # vérifier comment faire la show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
