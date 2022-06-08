fa66e33c28e8de75e46610de5022fc4672419924
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-08 15:50:50 +0200

    new def in private for scrapping, preparing background job

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -4,69 +4,44 @@ class HousesController < ApplicationController
   require 'nokogiri'
 
   def index
+    scrapping_lilloise
     # c'est pour vérifier si on récupère bien les params de district, puis a retirer
     @houses_params = params["house"]
 
     @houses = House.all
-
     # récupérer les bons paramètres pour district et price
     # district = district.name
     # price = house.price
     # ("https://lalilloise-immobilier.fr/annonces/?type=&localite=#{district}&prix=#{price}")
-
-    @results = []
-
-    html_content = URI.open("https://lalilloise-immobilier.fr/annonces/")
-    html_doc = Nokogiri::HTML(html_content)
-    html_doc.search('.card').each do |element|
-      square_meter = element.search(".annonce-infos li").first.text
-      room_number = element.search(".annonce-infos li")[1].text
-      name = element.search(".card-content .cat-name").text
-      description = element.search(".annonce-accroche").text
-      price = element.search(".price").text
-      link = element.search(".card-link")[0]['href']
-      begin
-      html_doc_unique = Nokogiri::HTML(URI.open(link))
-      rescue OpenURI::HTTPError => e
-        puts "Can't access #{link}"
-        puts e.message
-        puts
-        next
-      end
-      # html_content_unique = URI.open(link)
-      # html_doc_unique = Nokogiri::HTML(html_content_unique)
-      url_image = element.search(".attachment-annonce-thumb").attr('data-src').value.gsub(/400x200/, '640x480')
-
-      if (square_meter.to_i >= 50) && (room_number.to_i >= 3) && (price >= "100 000 €")
-        @results << [name: name, description: description, url_image: url_image, square_meter: square_meter, room_number: room_number, price: price, link: link]
-      end
-    end
   end
+
+  private
 
   def lilloise_url
     html_content = URI.open("https://lalilloise-immobilier.fr/annonces/")
-    @url = Nokogiri::HTML(html_content)
+    Nokogiri::HTML(html_content)
   end
 
   def scrapping_lilloise
-    html_doc.search('.card').each do |element|
+    lilloise_url.search('.card').each do |element|
       square_meter = element.search(".annonce-infos li").first.text
       room_number = element.search(".annonce-infos li")[1].text
       name = element.search(".card-content .cat-name").text
       description = element.search(".annonce-accroche").text
       price = element.search(".price").text
       link = element.search(".card-link")[0]['href']
-      begin
-      html_doc_unique = Nokogiri::HTML(URI.open(link))
-      rescue OpenURI::HTTPError => e
-        puts "Can't access #{link}"
-        puts e.message
-        puts
-        next
-      end
+      rescue_link(link)
       url_image = element.search(".attachment-annonce-thumb").attr('data-src').value.gsub(/400x200/, '640x480')
-      House.create[name: name, description: description, url_image: url_image, square_meter: square_meter, room_number: room_number, price: price, link: link]
+      House.create[name: name, description: description, image: url_image, square_meters: square_meter, room_number: room_number, price: price, link: link]
     end
+  end
+
+  def rescue_link(link_url)
+    Nokogiri::HTML(URI.open(link_url))
+  rescue OpenURI::HTTPError => e
+    puts "Can't access #{link_url}"
+    puts e.message
+    puts
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_092015) do
+ActiveRecord::Schema.define(version: 2022_06_08_130544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_06_02_092015) do
     t.bigint "district_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "link"
     t.index ["district_id"], name: "index_houses_on_district_id"
   end
 
