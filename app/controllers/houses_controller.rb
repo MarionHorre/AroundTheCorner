137124046a70c5fb1e6c2018a5d5263f1dec3602
Marion Horre <marion.horre@gmail.com>
2022-06-08 16:47:47 +0200

    Method not ready yet

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -32,19 +32,18 @@ class HousesController < ApplicationController
     price = element.search(".price").text
     link = element.search(".card-link")[0]['href']
     begin
-      html_doc_unique = Nokogiri::HTML(URI.open(link))
+      Nokogiri::HTML(URI.open(link))
     rescue OpenURI::HTTPError => e
-      puts "Can't access #{link }"
+      puts "Can't access #{link}"
       puts e.message
       puts
       next
     end
-    # html_content_unique = URI.open(link)
-    # html_doc_unique = Nokogiri::HTML(html_content_unique)
 
     url_image = element.search(".attachment-annonce-thumb").attr('data-src').value.gsub(/400x200/, '640x480')
 
       if (square_meter.to_i >= square_meters_form) && (room_number.to_i >= rooms_number_form) && ( minimal_price_form < price.to_i) && ( price.to_i <= maximal_price_form )
+
         @results << [name: name, description: description, url_image: url_image, square_meter: square_meter, room_number: room_number, price: price, link: link]
     end
 
@@ -52,8 +51,6 @@ class HousesController < ApplicationController
   end
 end
 
-
-# {"min_price"=>"0", "number_of_room"=>"2", "max_price"=>"250000", "square_meter"=>"30"}
 
 
 # methode pour avoir seulement les annonces avec ou + le nombres de pièces demandés
