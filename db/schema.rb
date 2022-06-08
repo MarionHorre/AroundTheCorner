fa26c060ad7de2b590f2da5a4439a33ce41354dd
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-08 15:56:22 +0200

    changed houses types to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_130544) do
+ActiveRecord::Schema.define(version: 2022_06_08_135236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 2022_06_08_130544) do
     t.string "name"
     t.string "image"
     t.string "description"
-    t.integer "price"
-    t.integer "square_meters"
-    t.integer "room_number"
+    t.string "price"
+    t.string "square_meters"
+    t.string "room_number"
     t.bigint "district_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
