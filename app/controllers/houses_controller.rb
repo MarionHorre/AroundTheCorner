d0c2f100ca7520b8112fcf48dee3944410a4bec1
Marion Horre <marion.horre@gmail.com>
2022-06-08 15:30:31 +0200

    Params récupérés et mis dans des variables

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -4,8 +4,13 @@ class HousesController < ApplicationController
   require 'nokogiri'
 
   def index
-    # c'est pour vérifier si on récupère bien les params de district, puis a retirer
-    @houses_params = params["house"]
+
+    #Hash par catégorie avec les params du form
+    square_meters_form = params[:house]['square_meter'].to_i
+    rooms_number_form = params[:house]['number_of_room'].to_i
+    minimal_price_form = params[:house]['min_price'].to_i
+    maximal_price_form = params[:house]['max_price'].to_i
+
 
     @houses = House.all
 
@@ -39,7 +44,7 @@ class HousesController < ApplicationController
 
     url_image = element.search(".attachment-annonce-thumb").attr('data-src').value.gsub(/400x200/, '640x480')
 
-      if (square_meter.to_i >= 50) && (room_number.to_i >= 3) && (price >= "100 000 €")
+      if (square_meter.to_i >= square_meters_form) && (room_number.to_i >= rooms_number_form) && ( minimal_price_form < price.to_i) && ( price.to_i <= maximal_price_form )
         @results << [name: name, description: description, url_image: url_image, square_meter: square_meter, room_number: room_number, price: price, link: link]
     end
 
@@ -47,6 +52,8 @@ class HousesController < ApplicationController
   end
 end
 
+
+# {"min_price"=>"0", "number_of_room"=>"2", "max_price"=>"250000", "square_meter"=>"30"}
 
 
 # methode pour avoir seulement les annonces avec ou + le nombres de pièces demandés
