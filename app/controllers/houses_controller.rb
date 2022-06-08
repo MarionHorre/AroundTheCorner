9c6f7a2a6bbcd2c41ce7bb9d4adec623bcdcd3c8
Marion Horre <marion.horre@gmail.com>
2022-06-08 11:46:19 +0200

    Fixed ugly img to be replaced with better resolution

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -25,9 +25,25 @@ class HousesController < ApplicationController
     description = element.search(".annonce-accroche").text
     price = element.search(".price").text
     link = element.search(".card-link")[0]['href']
-    url_image = element.search(".attachment-annonce-thumb").attr('data-src').value
+    begin
+      html_doc_unique = Nokogiri::HTML(URI.open(link))
+      galerie_img = html_doc_unique.search('.galerie-image')
+      if galerie_img.size > 0
+        thumbnail = galerie_img.attr('srcset').value
+      end
+    rescue OpenURI::HTTPError => e
+      puts "Can't access #{link }"
+      puts e.message
+      puts
+      next
+    end
+    # html_content_unique = URI.open(link)
+    # html_doc_unique = Nokogiri::HTML(html_content_unique)
+
+    url_image = element.search(".attachment-annonce-thumb").attr('data-src').value.gsub(/400x200/, '640x480')
+
       if (square_meter.to_i >= 50) && (room_number.to_i >= 3) && (price >= "100 000 €")
-        @results << [name: name, description: description, url_image: url_image, square_meter: square_meter, room_number: room_number, price: price, link: link]
+        @results << [name: name, description: description, url_image: url_image, square_meter: square_meter, room_number: room_number, price: price, link: link, thumbnail: thumbnail]
     end
 
     end
