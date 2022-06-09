d4b0e2ba0d1f31a13bf85536b47580e1c78522a9
Jordane Celet <mrjocte@gmail.com>
2022-06-09 17:18:19 +0200

    added scrapping in seed and district reference to house

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,8 @@
 require "json"
 require 'geokit'
 require_relative "polygone_maker"
+require 'open-uri'
+require 'nokogiri'
 
 District.destroy_all
 Interest.destroy_all
@@ -286,6 +288,47 @@ daily_kids_park = type_daily_kids_park.map do |element|
     interest = Interest.create!(address: adresse, longitude: longitude, latitude: latitude, type: kids_park, district: district)
   end
 end
+
+  def rescue_link(link_url)
+    Nokogiri::HTML(URI.open(link_url))
+    rescue OpenURI::HTTPError => e
+      puts "Can't access #{link_url}"
+      puts e.message
+  end
+
+  html_doc = URI.open("https://lalilloise-immobilier.fr/annonces/")
+  html_parsed = Nokogiri::HTML(html_doc)
+
+  html_parsed.search('.card').each do |element|
+    square_meter = element.search(".annonce-infos li").first.text.match(/\d{2,3}\b(,)/)[0][0..-2].to_i
+    room_number = element.search(".annonce-infos li")[1].text.to_i
+    name = element.search(".card-content .cat-name").text
+    description = element.search(".annonce-accroche").text
+    price = element.search(".price").text[0..-2].gsub(" ", "").to_i
+    link = element.search(".card-link")[0]['href']
+    rescue_link(link)
+    unless House.find_by(name: name, description: description, price: price)
+      url_image = element.search(".attachment-annonce-thumb").attr('data-src').value.gsub(/400x200/, '640x480')
+      begin
+        file = URI.open(url_image)
+        doc = Nokogiri::HTML(file) do
+          url_image = url_image
+        end
+      rescue OpenURI::HTTPError => e
+        if e.message == '404 Not Found'
+          url_image = url_image.gsub(/640x480/, '400x200')
+        else
+          raise e
+        end
+      end
+      House.create!(name: name, description: description, image: url_image,
+                    square_meters: square_meter, room_number: room_number, price: price,
+                    link: link)
+    end
+  end
+
+
+
 
 puts 'Finished!'
 # Interest.create(adresse, )
