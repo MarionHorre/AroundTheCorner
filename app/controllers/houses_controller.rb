489067925ea99407d2ee3c5ed78c1b580ff1f1a9
Marion Horre <marion.horre@gmail.com>
2022-06-09 11:13:24 +0200

    Method to search for houses with criterias working

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -14,7 +14,8 @@ class HousesController < ApplicationController
     maximal_price_form = params[:house]['max_price'].to_i
 
 
-    @houses = House.all
+    @houses = House.where( "CAST(square_meters AS int) >= ? AND CAST(room_number AS int) >= ? AND CAST(price AS int) BETWEEN ? AND ?", square_meters_form, rooms_number_form, minimal_price_form, maximal_price_form)
+
     # récupérer les bons paramètres pour district et price
     # district = district.name
     # price = house.price
@@ -31,11 +32,11 @@ class HousesController < ApplicationController
   def scrapping_lilloise
     House.destroy_all
     lilloise_url.search('.card').each do |element|
-      square_meter = element.search(".annonce-infos li").first.text
-      room_number = element.search(".annonce-infos li")[1].text
+      square_meter = element.search(".annonce-infos li").first.text.match(/\d{2,3}\b(,)/)[0][0..-2].to_i
+      room_number = element.search(".annonce-infos li")[1].text.to_i
       name = element.search(".card-content .cat-name").text
       description = element.search(".annonce-accroche").text
-      price = element.search(".price").text
+      price = element.search(".price").text[0..-2].gsub(" ", "").to_i
       link = element.search(".card-link")[0]['href']
       rescue_link(link)
       url_image = element.search(".attachment-annonce-thumb").attr('data-src').value.gsub(/400x200/, '640x480')
@@ -43,9 +44,6 @@ class HousesController < ApplicationController
                     square_meters: square_meter, room_number: room_number, price: price,
                     link: link)
     end
-    if (square_meter.to_i >= square_meters_form) && (room_number.to_i >= rooms_number_form) && ( minimal_price_form < price.to_i) && ( price.to_i <= maximal_price_form )
-
-      @results << [name: name, description: description, url_image: url_image, square_meter: square_meter, room_number: room_number, price: price, link: link]
   end
 
   def rescue_link(link_url)
@@ -56,19 +54,3 @@ class HousesController < ApplicationController
     puts
   end
 end
-
-
-
-# methode pour avoir seulement les annonces avec ou + le nombres de pièces demandés
-# @room_numbers.each do |room|
-
-# end
-
-# methode pour avoir seulement les annonces avec ou + le nombres de m² demandés
-# @square_meters.each do |meter|
-#   if
-#     p meter
-#   end
-# end
-
-# if room.to_i >= 3 && meter.to_i >= 70
